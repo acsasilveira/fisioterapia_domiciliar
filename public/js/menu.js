844e8c69bd8c93b6d0a9b2fa0907d0b508010ad2
acsasilveira.ti <acsasilveira.ti@gmail.com>
2025-11-24 21:26:43 +0000

    Navbar

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,8 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const dashboardNav = document.querySelector('.dashboard-nav');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger-menu');
+  const navMenu = document.querySelector('.nav-menu');
 
-    hamburgerMenu.addEventListener('click', function () {
-        dashboardNav.classList.toggle('active');
+  if (hamburger && navMenu) {
+    hamburger.addEventListener('click', (event) => {
+      event.stopPropagation();
+      navMenu.classList.toggle('open');
+      hamburger.classList.toggle('open');
     });
+
+    document.addEventListener('click', (event) => {
+      if (navMenu.classList.contains('open')) {
+        const isClickInsideNav = navMenu.contains(event.target);
+        const isClickOnHamburger = hamburger.contains(event.target);
+
+        if (!isClickInsideNav && !isClickOnHamburger) {
+          navMenu.classList.remove('open');
+          hamburger.classList.remove('open');
+        }
+      }
+    });
+
+    const navItems = navMenu.querySelectorAll('.nav-item');
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            if (navMenu.classList.contains('open')) {
+                navMenu.classList.remove('open');
+                hamburger.classList.remove('open');
+            }
+        });
+    });
+  }
 });
