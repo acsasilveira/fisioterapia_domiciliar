1c3f869b981a4840179a432ca86fdabf9a5a637c
acsasilveira.ti <acsasilveira.ti@gmail.com>
2025-11-25 11:22:45 +0000

    Navbar

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,6 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger-menu');
   const navMenu = document.querySelector('.nav-menu');
 
+  // Lista de páginas onde o menu hambúrguer não deve aparecer
+  const restrictedPages = [
+    '/pages/admin-login.html',
+    '/pages/cadastro-depoimentos.html',
+    '/pages/cadastro-produtos.html',
+    '/pages/dash-depoimentos.html',
+    '/pages/dash-geral.html',
+    '/pages/dash-produtos.html'
+  ];
+
+  // Verifica se a página atual está na lista de páginas restritas
+  const isRestrictedPage = restrictedPages.some(page => window.location.pathname.endsWith(page));
+
+  // Se estiver em uma página restrita, oculta o menu hambúrguer
+  if (isRestrictedPage) {
+    if (hamburger) {
+      hamburger.style.display = 'none';
+    }
+    return; // Impede a execução do restante do código
+  }
+
   if (hamburger && navMenu) {
     hamburger.addEventListener('click', (event) => {
       event.stopPropagation();
